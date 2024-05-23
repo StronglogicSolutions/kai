1ec1f74f26c8f2275ba30183da6b2ee5c831ce14
Emmanuel Buckshi <adventurist@gmail.com>
2024-05-22 21:09:57 -0400

    moving away from console input prompt, and instead doing everything through IPC

--- src/kai.hpp
+++ src/kai.hpp
@@ -10,6 +10,8 @@ using namespace kiq::log;
 //--------------------------------------------------------
 static const char* url = "http://127.0.0.1:8080/completion"; // LLAMA AI SERVER
 //--------------------------------------------------------
+static const uint32_t g_id = 123;
+//--------------------------------------------------------
 struct session
 {
   using exchange_t = std::pair<std::string, std::string>;
@@ -19,7 +21,7 @@ struct session
 };
 
 using sessions_t = std::map<uint32_t, session>;
-
+using messages_t = std::map<std::string, kiq::ipc_message::u_ipc_msg_ptr>;
 class kai
 {
  public:
@@ -28,16 +30,41 @@ class kai
   : endpoint_([this](kiq::ipc_message::u_ipc_msg_ptr msg)
   {
     using handler_t = std::map<uint8_t, std::function<void(kiq::ipc_message::u_ipc_msg_ptr)>>;
+
+    static const auto last_msg_idx = std::to_string(std::numeric_limits<uint32_t>::max());
+
     klog().t("Message received:\n{}", msg->to_string());
 
     const auto handler = handler_t
     {
-      { kiq::constants::IPC_KIQ_MESSAGE,  [this](const auto ipc_msg)
+      { kiq::constants::IPC_KIQ_MESSAGE,  [this](auto&& ipc_msg)
         {
-          generate(get_payload(static_cast<kiq::kiq_message*>(ipc_msg.get())->payload()).query());
+          const auto payload = get_payload(static_cast<kiq::kiq_message*>(ipc_msg.get())->payload());
+
+          if (payload.type() == "generate")
+          {
+            generate(payload.data(), last_msg_idx);
+            messages_.insert_or_assign(last_msg_idx, std::move(ipc_msg));
+          }
+          else
+            klog().w("Received unknown request name: {}", payload.type());
         }
       },
-      { kiq::constants::IPC_STATUS,       [this](const auto ipc_msg)
+      { kiq::constants::IPC_PLATFORM_TYPE,  [this](auto&& ipc_msg)
+        {
+          const auto p_msg   = static_cast<kiq::platform_message*>(ipc_msg.get());
+          const auto payload = get_payload(p_msg->args());
+
+          if (payload.type() == "generate")
+          {
+            generate(payload.data());
+            messages_.insert_or_assign(p_msg->id(), std::move(ipc_msg));
+          }
+          else
+            klog().w("Received unknown request name: {}", payload.type());
+        }
+      },
+      { kiq::constants::IPC_STATUS,       [this](auto&& ipc_msg)
         {
           endpoint_.send_ipc_message(std::make_unique<kiq::okay_message>());
           endpoint_.connect();
@@ -57,37 +84,18 @@ class kai
   {}
 
   //-----------------------------------------------------------------------------
-  void add(uint32_t id)
+  void add(std::string_view id, const session::exchange_t& exchange)
   {
-    if (has_session(id))
-      klog().w("Session {} already exists. Ignored.", id);
-    else
-      sessions_.insert({id, session{}});
-  }
-  //-----------------------------------------------------------------------------
-  void add(uint32_t id, const session::exchange_t& exchange)
-  {
-    if (!has_session(id))
-      add(id);
-    sessions_.at(id).dialogue.push_back(exchange);
+    if (!has_session(g_id))
+      sessions_.insert({g_id, session{}});
+    sessions_.at(g_id).dialogue.push_back(exchange);
+
+    const auto ipc_msg = messages_.at(id.data()).get();
 
     endpoint_.send_ipc_message(std::make_unique<kiq::platform_info>(
       "kai",
       std::string{exchange.first + "\n\n" + exchange.second},
       "generated"));
-  }
-  //-----------------------------------------------------------------------------
-  session::exchange_t
-  find(uint32_t id, const std::string& query) const
-  {
-    if (!has_session(id))
-      klog().w("Can't find dialogue: session {} doesn't exist", id);
-    else
-      for (const auto& exchange : sessions_.at(id).dialogue)
-        if (exchange.first.find(query) != std::string::npos)
-          return exchange;
-
-    return session::exchange_t{};
   }
   //-----------------------------------------------------------------------------
   void
@@ -102,9 +110,11 @@ class kai
           id, exchange.first, exchange.second);
   }
   //-----------------------------------------------------------------------------
-  void generate(std::string_view s)
+  void generate(std::string_view s, std::string_view id = "")
   {
-    post(s, url);
+    const auto response = post(s, url);
+    const auto value    = decode(response);
+    add(id, session::exchange_t{s, value});
   }
 
  private:
@@ -114,4 +124,5 @@ class kai
   //--------------------------
   sessions_t sessions_;
   endpoint   endpoint_;
+  messages_t messages_;
 };
